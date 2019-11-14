054848682d46128d1042656b5a5ee0749c43f98b
Joshua Chan <JChan@ucvts.tec.nj.us>
2019-11-14 12:35:34 -0500

    Sum

--- src/sum.js
+++ src/sum.js
@@ -1,2 +1,25 @@
 const readlineSync = require("readline-sync");
-let 
+
+let sum = 0;
+let x = 0;
+
+console.log("")
+do{
+  lowerBound = Number(readlineSync.question("Lower bound: "));
+  upperBound = Number(readlineSync.question("Upper bound: "));
+} while (lowerBound < Number.MIN_SAFE_INTEGER || upperBound > Number.MAX_SAFE_INTEGER || lowerBound >= upperBound || Number.isNaN(lowerBound) || Number.isNaN(upperBound) || lowerBound % 1 != 0 || upperBound % 1 != 0)
+
+for (let x = lowerBound; x <= upperBound; x++) {
+if (lowerBound % 2 === 0){
+  for (x = lowerBound; x <= upperBound; x+=2){
+    sum = x + sum;
+  }
+}
+}
+if (lowerBound % 2 !== 0){
+  for (x = lowerBound + 1; x <= upperBound; x+=2){
+    sum = x + sum;
+  }
+}
+sum = sum.toLocaleString("en");
+console.log("\n" + sum + ".");
