0417d2a9378686984992bc47fa6a8f4410fd7bd3
Joshua Chan <JChan@ucvts.tec.nj.us>
2019-11-18 12:25:30 -0500

    reverse

---
+++ src/reverse.js
@@ -0,0 +1,18 @@
+const readlineSync = require("readline-sync");
+let reversed = "";
+let x = 0
+console.log("")
+do{
+  positiveInteger = Number(readlineSync.question("Positive Integer: "));
+} while (positiveInteger < Number.MIN_SAFE_INTEGER || positiveInteger > Number.MAX_SAFE_INTEGER || Number.isNaN(positiveInteger) || positiveInteger % 1 != 0 || positiveInteger <= 0)
+
+do {
+  x = positiveInteger % 10;
+  reversed = reversed + x + ", ";
+  positiveInteger = positiveInteger - x
+  positiveInteger = positiveInteger/10;
+} while (positiveInteger > 10);
+
+reversed = reversed + positiveInteger + "."
+
+console.log(reversed);
